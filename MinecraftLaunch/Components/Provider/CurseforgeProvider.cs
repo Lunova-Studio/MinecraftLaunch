0b27eff31d76a3eebb94f2c715b735d9b96676e7
YangSpring429 <3424968114@qq.com>
2025-11-21 20:36:12 +0800

    fix：修复一处小 Bug

--- MinecraftLaunch/Components/Provider/CurseforgeProvider.cs
+++ MinecraftLaunch/Components/Provider/CurseforgeProvider.cs
@@ -118,7 +118,7 @@ public sealed class CurseforgeProvider {
                 sortField = searchOptions.SortField,
                 classId = searchOptions.ClassId,
                 gameVersion = searchOptions.GameVersion,
-                searchFilter = HttpUtility.UrlEncode(searchOptions.GameVersion)
+                searchFilter = HttpUtility.UrlEncode(searchOptions.SearchFilter)
             });
 
         var modLoaderType = searchOptions.ModLoaderType;
