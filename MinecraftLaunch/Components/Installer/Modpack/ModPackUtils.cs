ecaaf705c526c3e4a06b831da497bf299f875ff5
Yang429 <3424968114@qq.com>
2026-04-11 23:02:47 +0800

    fix：修复一处因升级运行时导致构建失败的问题

--- MinecraftLaunch/Components/Installer/Modpack/ModPackUtils.cs
+++ MinecraftLaunch/Components/Installer/Modpack/ModPackUtils.cs
@@ -1,44 +1,43 @@
 ﻿using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO.Compression;
-using System.Net.Sockets;
-using MinecraftLaunch.Extensions;
+
+#if NET10_0_OR_GREATER
+using ZipExt = System.IO.Compression.ZipFileExtensions;
+#else
+using ZipExt = MinecraftLaunch.Extensions.ZipArchiveExtension;
+#endif
 
 namespace MinecraftLaunch.Components.Installer.Modpack;
 
-internal static class ModPackUtils
-{
+internal static class ModPackUtils {
     public const char ZipPathSeparator = '/';
-    
+
 
     public static async Task ExtractSingleThreadAsync(
         string srcZipPath,
         string overridesPrefix,
         string independentAndFullWorkingPath,
         /*执行线程不保证*/Action<ZipArchive> whenEachEntryCompleted = null,
-        CancellationToken cancellationToken = default)
-    {
+        CancellationToken cancellationToken = default) {
         Debug.Assert(srcZipPath is not null || overridesPrefix is not null || independentAndFullWorkingPath is not null);
-        cancellationToken.ThrowIfCancellationRequested(); 
+        cancellationToken.ThrowIfCancellationRequested();
         using var zip = ZipFile.OpenRead(srcZipPath);
-        foreach (var item in zip.Entries)
-        {
+        foreach (var item in zip.Entries) {
             cancellationToken.ThrowIfCancellationRequested();
             // 排除非 <overrides>/文件
             if (!item.FullName.StartsWith(overridesPrefix, StringComparison.OrdinalIgnoreCase)) continue;
             var targetPath = Path.Combine(
                 independentAndFullWorkingPath,
                 RemoveOverridesPrefix(item.FullName, overridesPrefix));
-            if (item.FullName.EndsWith(ZipPathSeparator))
-            {
+            if (item.FullName.EndsWith(ZipPathSeparator)) {
                 Directory.CreateDirectory(targetPath);
                 continue;
             }
             if (!IsShouldExtract(item, overridesPrefix)) continue;
-            await item.ExtractToFileAsync(
-                targetPath,
-                overwrite:true,cancellationToken
-            ).ConfigureAwait(false);
+
+            await ZipExt.ExtractToFileAsync(
+                item, targetPath, true, cancellationToken).ConfigureAwait(false);
+
             whenEachEntryCompleted?.Invoke(zip);
         }
     }
@@ -47,8 +46,7 @@ internal static class ModPackUtils
 
     private static bool IsShouldExtract(
         ZipArchiveEntry entry,
-        string overridesPrefix)
-    {
+        string overridesPrefix) {
         // 排除目录
         if (entry.FullName.EndsWith(ZipPathSeparator)) return false;
         // 排除非 <overrides>/文件
@@ -59,10 +57,8 @@ internal static class ModPackUtils
     // 修正路径
     private static string RemoveOverridesPrefix(
         string source,
-        string overridesPrefix)
-    {
-        if (overridesPrefix.EndsWith('/'))
-        {
+        string overridesPrefix) {
+        if (overridesPrefix.EndsWith('/')) {
             return source[overridesPrefix.Length..];
         }
 
