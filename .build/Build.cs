0650e70295c298fca770ab17d335313dcf53e1f8
Yang429 <3424968114@qq.com>
2026-04-14 00:52:17 +0800

    ci：重写 CI 工作流

---
+++ .build/Build.cs
@@ -0,0 +1,88 @@
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Components;
+
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using System.Collections.Generic;
+using Serilog;
+using Nuke.Common.CI;
+using Nuke.Common.Tooling;
+
+[GitHubActions("ci_build",
+    GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = true,
+    OnPushBranches = ["4.0.x"],
+    InvokedTargets = [nameof(IPublish.Publish)],
+    ImportSecrets = [nameof(NugetKey)])]
+class Build : NukeBuild, IHazSolution, ITest, IPack, ICompile, IRestore, IPublish {
+    private readonly AbsolutePath _output = RootDirectory / "artifacts";
+
+    [Parameter(Name = "NUGET_API_KEY"), Secret] private readonly string NugetKey;
+
+    [Solution(GenerateProjects = true)]
+    private readonly Solution Solution;
+
+    private readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    string IPublish.NuGetApiKey => NugetKey;
+
+    Nuke.Common.ProjectModel.Solution IHazSolution.Solution => Solution;
+
+    AbsolutePath IPack.PackagesDirectory => RootDirectory;
+    
+    public IEnumerable<Project> TestProjects => Solution.AllProjects;
+
+    public static int Main() => Execute<Build>(x => ((IPack)x).Pack);
+
+    private T From<T>() where T : INukeBuild => (T)(object)this;
+
+    #region Targets
+
+    Target Clean => _ => _
+        .Executes(() => {
+            _output.CreateOrCleanDirectory();
+            Log.Information("clear over!");
+        });
+
+    Target IRestore.Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() => {
+            DotNetRestore(s => s.SetProjectFile(Solution));
+        });
+
+    Target ICompile.Compile => _ => _
+        .TryDependsOn<IRestore>()
+        .Executes(() => {
+            DotNetBuild(x => x
+                .SetProjectFile(Solution.MinecraftLaunch)
+                .SetConfiguration(Configuration.Release)
+                .EnableNoRestore());
+        });
+
+    Target ITest.Test => _ => _
+        .Inherit<ITest>()
+        .TryDependsOn<ICompile>();
+
+    Target IPack.Pack => _ => _
+        .TryDependsOn<ITest>()
+        .Executes(() => {
+            DotNetPack(x => x
+                .SetConfiguration(Configuration.Release)
+                .SetOutputDirectory(_output)
+                .CombineWith([Solution.MinecraftLaunch, Solution.MinecraftLaunch_Base], (s, proj) =>s
+                    .EnableNoBuild()
+                    .SetProject(proj)));
+        });
+
+    Target IPublish.Publish => _ => _
+        .Inherit<IPublish>()
+        .Consumes(From<IPack>().Pack)
+        .OnlyWhenStatic(() => IsServerBuild);
+
+    #endregion
+}
+//Clean → Restore → Compile → Pack → Push
